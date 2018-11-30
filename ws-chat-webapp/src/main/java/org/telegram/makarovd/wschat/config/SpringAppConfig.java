3dd43021e5f8dc9a120d9246e1fc9ae3e237938b
makarovd89 <makarovd89@yahoo.com>
2018-11-30 16:31:49 +0600

    Add spring security

--- ws-chat-webapp/src/main/java/org/telegram/makarovd/wschat/config/SpringAppConfig.java
+++ ws-chat-webapp/src/main/java/org/telegram/makarovd/wschat/config/SpringAppConfig.java
@@ -1,7 +1,9 @@
 package org.telegram.makarovd.wschat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import(SpringSecurityConfig.class)
 @Configuration
 public class SpringAppConfig {
 }
